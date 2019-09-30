76b172ff8430eab6b4ae90cf7188dbcc9da74734
Eleimak <arthurtatarin@gmail.com>
2019-09-30 21:09:06 +0300

    Commit from community

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("Hello word!");
         System.out.println("Hello word!");
         System.out.println("Hello word!");
+        System.out.println("Hello word!");
     }
 }
